c0c7f6615637f17abb126bd3865389cebd482405
PawelSzy <peal1982@gmail.com>
2019-08-15 17:16:44 +0200

    Add slick slider.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
-
+$(document).ready(function(){
+    $('.carousel-slider').slick({
+        arrows: true,
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+    });
+});
