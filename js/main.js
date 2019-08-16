1c6f52cf54e08cba3a0ac67849edbfa11c643b10
PawelSzy <peal1982@gmail.com>
2019-08-16 17:10:20 +0200

    Change slick slider arrows

--- js/main.js
+++ js/main.js
@@ -4,5 +4,11 @@ $(document).ready(function(){
         infinite: true,
         slidesToShow: 4,
         slidesToScroll: 1,
+        prevArrow: '<div class="slick-prev fas fa-angle-left slick-arrow" aria-label="Previous" style="display: block;"></div>',
+        nextArrow: '<div class="slick-next fas fa-angle-right slick-arrow" aria-label="Previous" style="display: block;"></div>',
+
+        //prevArrow: '<button class="slick-prev fa fa-arrow-right" aria-label="Previous" type="button" style="display: block;">Previous</button>',
+        //nextArrow: '<i class="fa fa-arrow-right"></i>',
+        //prevArrow: '<i class="fa fa-arrow-left"></i>',
     });
 });
