c8f0fc2266fd17fe60a472ab7f9a540d1480f2c5
PawelSzy <peal1982@gmail.com>
2019-08-19 14:01:22 +0200

    Mobile look of elements first draft

--- js/main.js
+++ js/main.js
@@ -6,7 +6,31 @@ $(document).ready(function(){
         slidesToScroll: 1,
         prevArrow: '<div class="slick-prev fas fa-angle-left slick-arrow" aria-label="Previous" style="display: block;"></div>',
         nextArrow: '<div class="slick-next fas fa-angle-right slick-arrow" aria-label="Previous" style="display: block;"></div>',
-        variableWidth: true,
+        //variableWidth: true,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    infinite: true,
+                }
+            },
+            {
+                breakpoint: 720,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
     });
     $('.explore-link__outside_link').click(function() {
         console.log('test');
